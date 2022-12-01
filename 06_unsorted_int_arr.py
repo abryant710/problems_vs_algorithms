9459e139167b0044e3673eff79be087bf9ae0463
Alex Bryant <alexbryant710@gmail.com>
2022-12-02 03:45:53 +1100

    Finished exercise 6

--- 06_unsorted_int_arr.py
+++ 06_unsorted_int_arr.py
@@ -7,22 +7,37 @@ Bonus Challenge: Is it possible to find the max and min in a single traversal?
 Sorting usually requires O(n log n) time Can you come up with a O(n) algorithm (i.e., linear time)?
 
 """
+import random
 
 
 def get_min_max(ints):
     """
     Return a tuple(min, max) out of list of unsorted integers.
 
+    Time complexity: O(n)
+    since we are iterating through the list only once
+    Space complexity: O(1)
+    since we are not using any additional data structures
+
     Args:
        ints(list): list of integers containing one or more integers
     """
-    pass
+    min = ints[0]
+    max = ints[0]
+    for i in ints:
+        if i < min:
+            min = i
+        if i > max:
+            max = i
+    return (min, max)
 
 
-## Example Test Case of Ten Integers
-import random
+def test():
+    l = [i for i in range(0, 10)]  # a list containing 0 - 9
+    random.shuffle(l)
 
-l = [i for i in range(0, 10)]  # a list containing 0 - 9
-random.shuffle(l)
+    assert (0, 9) == get_min_max(l), print("Failed test")
+    print("Passed test")
 
-print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+
+test()
