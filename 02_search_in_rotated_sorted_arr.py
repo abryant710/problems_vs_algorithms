0e7e7497e1a9f84a66bf8be939dc99c36f7b8447
Alex Bryant <alexbryant710@gmail.com>
2022-12-01 19:50:33 +1100

    Finished exercise 2

--- 02_search_in_rotated_sorted_arr.py
+++ 02_search_in_rotated_sorted_arr.py
@@ -20,12 +20,34 @@ def rotated_array_search(input_list, number):
     """
     Find the index by searching in a rotated sorted array
 
+    Time complexity: O(log n)
+    since we are splitting the array in half each time
+    which is equivalent to using a binary search, which is O(log n)
+    Space complexity: O(1)
+    since we are not using any additional data structures to store data
+
     Args:
       input_list(array), number(int): Input array to search and the target
     Returns:
       int: Index or -1
     """
-    pass
+    start = 0
+    end = len(input_list) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if input_list[mid] == number:
+            return mid
+        if input_list[start] <= input_list[mid]:
+            if input_list[start] <= number < input_list[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            if input_list[mid] < number <= input_list[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+    return -1
 
 
 def linear_search(input_list, number):
@@ -35,17 +57,17 @@ def linear_search(input_list, number):
     return -1
 
 
-def test_function(test_case):
+def test_function(test_case, test_num):
     input_list = test_case[0]
     number = test_case[1]
-    if linear_search(input_list, number) == rotated_array_search(input_list, number):
-        print("Pass")
-    else:
-        print("Fail")
+    assert linear_search(input_list, number) == rotated_array_search(input_list, number), "Test {}: Failed".format(
+        test_num
+    )
+    print("Passed test case {}".format(test_num))
 
 
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 8])
-test_function([[6, 7, 8, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6], "01")
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1], "02")
+test_function([[6, 7, 8, 1, 2, 3, 4], 8], "03")
+test_function([[6, 7, 8, 1, 2, 3, 4], 1], "04")
+test_function([[6, 7, 8, 1, 2, 3, 4], 10], "05")
