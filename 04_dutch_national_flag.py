98f0735839dbae88ce93290acd865ba231fe19aa
Alex Bryant <alexbryant710@gmail.com>
2022-12-01 20:13:36 +1100

    Completed exercise 4 using a loop and hashing technique

--- 04_dutch_national_flag.py
+++ 04_dutch_national_flag.py
@@ -12,21 +12,31 @@ def sort_012(input_list):
     """
     Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
 
+    Time complexity: O(n)
+    since we traverse the input array once
+    Space complexity: O(n)
+    since we create a dictionary to store the counts of each number, which scales linearly with the size of the input array
+
     Args:
        input_list(list): List to be sorted
     """
-    pass
+    tallies = {0: 0, 1: 0, 2: 0}
+    for num in input_list:
+        if num == 0:
+            tallies[0] += 1
+        elif num == 1:
+            tallies[1] += 1
+        else:
+            tallies[2] += 1
+    return [0] * tallies[0] + [1] * tallies[1] + [2] * tallies[2]
 
 
-def test_function(test_case):
+def test_function(test_case, test_num):
     sorted_array = sort_012(test_case)
-    print(sorted_array)
-    if sorted_array == sorted(test_case):
-        print("Pass")
-    else:
-        print("Fail")
+    assert sorted_array == sorted(test_case), print("Failed test case {}".format(test_num))
+    print("Passed test {}".format(test_num))
 
 
-test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
-test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
-test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2], "01")
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1], "02")
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2], "03")
